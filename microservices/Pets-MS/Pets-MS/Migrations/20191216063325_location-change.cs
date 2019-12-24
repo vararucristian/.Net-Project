1b2546788bb12d06a743f3e64bba8a5e075e92fe
vararucristian <48356098+vararucristian@users.noreply.github.com>
2019-12-24 12:21:38 +0200

    session token/ validations for email and password

---
+++ microservices/Pets-MS/Pets-MS/Migrations/20191216063325_location-change.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pets_MS.Migrations
+{
+    public partial class locationchange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
