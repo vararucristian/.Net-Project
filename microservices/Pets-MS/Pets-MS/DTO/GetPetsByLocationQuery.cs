591e2e0d35817aa0b0ed5944976440602aac5ccf
anaturcu <48413771+anaturcu@users.noreply.github.com>
2020-01-06 17:31:03 +0200

    calcul distante

--- microservices/Pets-MS/Pets-MS/DTO/GetPetsByLocationQuery.cs
+++ microservices/Pets-MS/Pets-MS/DTO/GetPetsByLocationQuery.cs
@@ -3,17 +3,22 @@ using Pets_MS.Data;
 using System;
 using System.Collections.Generic;
 
+
 namespace Pets_Ms.Business.Commands
 {
     public class GetPetsByLocationQuery : IRequest<List<Dictionary<string, object>>>
     {
+        public Animal Species { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public double Distance { get; set; }
 
-        public GetPetsByLocationQuery(double latitude, double longitude)
+        public GetPetsByLocationQuery(Animal species, double latitude, double longitude, double distance)
         {
+            Species = species;
             Latitude = latitude;
             Longitude = longitude;
+            Distance = distance;
         }
 
     }
