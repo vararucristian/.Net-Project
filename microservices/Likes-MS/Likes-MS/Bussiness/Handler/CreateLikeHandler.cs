060cd3dda8acebdaec14d8e8599a713f0aeb0ed6
vararucristian <48356098+vararucristian@users.noreply.github.com>
2020-01-11 11:35:29 +0200

    email templat

--- microservices/Likes-MS/Likes-MS/Bussiness/Handler/CreateLikeHandler.cs
+++ microservices/Likes-MS/Likes-MS/Bussiness/Handler/CreateLikeHandler.cs
@@ -3,12 +3,15 @@ using Likes_MS.DTO;
 using MediatR;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Likes_MS.Bussiness.Handler
 {
+
+
     public class CreateLikeHandler : IRequestHandler<CreateLike, Dictionary<string, object>>
     {
         private readonly LikeContext LikeContext;
@@ -17,13 +20,49 @@ namespace Likes_MS.Bussiness.Handler
         {
             LikeContext = likeContext;
         }
+
+
+
+        private bool SendEmail(string eMail, string username)
+        {
+            bool emailSent = true;
+            try
+            {
+                string templatePath = "..\\..\\..\\templates\\mail.html";
+                string messageBody = System.IO.File.ReadAllText(templatePath);
+                messageBody = messageBody.Replace("{username}", username);
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress("happypet.application@gmail.com");
+                message.To.Add(new MailAddress(eMail));
+                message.Subject = "New Like";
+                message.IsBodyHtml = true;
+                message.Body = messageBody;
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("happypet.application@gmail.com", "happy12#pet");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception) {
+                emailSent = false;
+            }
+
+            return emailSent;
+        }
+
         public async Task<Dictionary<string, object>> Handle(CreateLike request, CancellationToken cancellationToken)
         {
             var response = new Dictionary<string, object>();
             var succes = true;
+            var emailSent = SendEmail(request.PersonEMail, request.Username);
             var like = Like.Create(request.PersonId, request.PetId);
-            try { 
-                
+            try
+            {
+
                 LikeContext.Likes.Add(like);
                 await LikeContext.SaveChangesAsync(cancellationToken);
             }
@@ -34,6 +73,7 @@ namespace Likes_MS.Bussiness.Handler
 
             response.Add("succes", succes);
             response.Add("like", like);
+            response.Add("emailSent", emailSent);
             return response;
 
         }
